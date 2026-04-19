c4ad704450e8d0a0e0249320c6fde6a2a5e2501e
Milad Khazaei <MiladKhazaei2015@gmail.com>
2026-04-19 22:30:41 +0330

    remove comments

--- ProductsWebApi/Models/Product.cs
+++ ProductsWebApi/Models/Product.cs
@@ -12,9 +12,6 @@ namespace ProductsWebApiDemo.Models
         public int? SellCount { get; set; }
         // Read-only properties
         public int? RemainingCount => TotalCount - SellCount;
-        [Column(TypeName = "decimal(10,2)")]
-        [DisplayName("Sell Price")]
-        [Display(Name = "Sell Price")]
         public decimal? SellPrice => SellCount * Price;
         public decimal? Price { get; set; }
     }
